fc0eab7596f555a0680009167710f6f4b97c404c
ituberus <34318384+ituberus@users.noreply.github.com>
2025-03-25 10:53:49 +0000

    Update payment.js

--- paytrack/payment.js
+++ paytrack/payment.js
@@ -469,7 +469,7 @@ function handlePaymentSuccess() {
 
         // 5) Redirect to "Thank you" page or wherever
         setTimeout(() => {
-          window.location.href = 'thanks.html';
+          window.location.href = 'https://ituberus.github.io/tesl/thanks';
         }, 500);
 
       } else {
